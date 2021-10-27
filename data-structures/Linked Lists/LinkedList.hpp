f8a692d57b2874db13ba3d613ee119518bd16359
tr3m431 <timewizard2500@gmail.com>
2021-10-27 17:42:57 -0400

    linked list methods added

--- data-structures/Linked Lists/LinkedList.hpp
+++ data-structures/Linked Lists/LinkedList.hpp
@@ -11,6 +11,14 @@ struct Node {
 };
 
 // insert
+template<typename T>
+void insert(Node<T>* head, T newNode, size_t position);
 // append
+template<typename T>
+void append(Node<T>* head, T newNode);
 // remove
+template<typename T>
+void remove(Node<T>* head, size_t position);
 // length
+template<typename T>
+size_t length(Node<T>* head);
